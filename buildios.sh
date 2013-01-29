7745cc8c457bcecd055756e608d84216e7ffd6f4
Ben Borowski <ben.borowski@typeoneerror.com>
2013-01-28 23:15:25 -0800

    Updated ios builders.

--- buildios.sh
+++ buildios.sh
@@ -3,6 +3,7 @@
 src="$( cd "$( dirname $0 )" && pwd )"
 
 echo "Cleaning iOS host..."
+echo ""
 $src/xcode/libmoai/clean.sh -c Release -p ios
 
 echo "Building iOS host..."
---
+++ openios.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+open /tmp/ios/default/libmoai/Release
