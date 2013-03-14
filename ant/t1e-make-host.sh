c843b2bc96f7b75efd6f4aedfd184e96444d1499
Sean Monahan <sean.monahan@typeoneerror.com>
2013-03-14 13:15:22 -0700

    Adds helper script for building the host with correct options

---
+++ ant/t1e-make-host.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+./make-host.sh -p com.typeoneerror --use-fmod true --use-untz false --disable-adcolony --disable-billing --disable-chartboost --disable-crittercism --disable-facebook --disable-tapjoy
