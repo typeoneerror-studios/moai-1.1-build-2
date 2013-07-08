3b8a2fd0a98fd359b3f233821a2b02730ccf9bf5
Sean Monahan <sean.monahan@typeoneerror.com>
2013-07-08 16:24:33 -0700

    Updates Android build scripts

---
+++ ant/t1e-make-host-untz.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+./make-host.sh -p com.typeoneerror --use-fmod false --use-untz true --disable-adcolony --disable-billing --disable-chartboost --disable-crittercism --disable-facebook --disable-tapjoy
